3102eeb7e49ba024009d6fb6ed3ff47af36f20d8
Deepika Periyasamy <deepikaperiyasamy502@gmail.com>
2026-04-22 16:14:57 +0530

    Time: 4 ms (92.77%) | Memory: 48.3 MB (20.45%) - LeetSync

--- 2254-check-if-every-row-and-column-contains-all-numbers/check-if-every-row-and-column-contains-all-numbers.java
+++ 2254-check-if-every-row-and-column-contains-all-numbers/check-if-every-row-and-column-contains-all-numbers.java
@@ -4,20 +4,24 @@ class Solution {
         int n = matrix.length;
 
         for(int i=0;i<n;i++){
-            HashSet<Integer> set = new HashSet<>();
+            // HashSet<Integer> set = new HashSet<>();
+            boolean[] arr = new boolean[n+1];
             for(int j=0;j<n;j++){
                 int num = matrix[i][j];
-                if(num<1 || num>n || set.contains(num)) return false;
-                set.add(num);
+                if(num<1 || num>n || arr[num]) return false;
+                // set.add(num); 
+                arr[num] = true;
             }
         }
 
         for(int j=0;j<n;j++){
-            HashSet<Integer> set = new HashSet<>();
+            // HashSet<Integer> set = new HashSet<>();
+            boolean[] arr = new boolean[n+1];
             for(int i=0;i<n;i++){
                 int num = matrix[i][j];
-                if(num<1 || num>n || set.contains(num)) return false;
-                set.add(num);
+                if(num<1 || num>n || arr[num]) return false;
+                // set.add(num);
+                arr[num] = true;
             }
         }
 
