cf20c37d2c95531fb4f782e75ce78e7551ff2026
Deepika Periyasamy <deepikaperiyasamy502@gmail.com>
2026-05-05 09:28:00 +0530

    Time: 1 ms (95.35%) | Memory: 43.2 MB (49.62%) - LeetSync

---
+++ 1894-merge-strings-alternately/merge-strings-alternately.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        
+        StringBuilder sb = new StringBuilder();
+        int left = 0, right = 0;
+        while(left<word1.length() && right < word2.length()){
+            sb.append(word1.charAt(left)).append(word2.charAt(right));
+            left++;
+            right++;
+        } 
+
+        if(left!=word1.length()) sb.append(word1.substring(left));
+        if(right!=word2.length()) sb.append(word2.substring(right));
+
+        return sb.toString();
+    }
+}
