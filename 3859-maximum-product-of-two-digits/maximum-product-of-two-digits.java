11c3963b3a4e717785f9fd0e463ce3e36734233d
Deepika Periyasamy <deepikaperiyasamy502@gmail.com>
2026-07-25 14:39:38 +0530

    Time: 1 ms (100.00%) | Memory: 42.5 MB (85.43%) - LeetSync

--- 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.java
+++ 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.java
@@ -1,34 +1,52 @@
 class Solution {
     public int maxProduct(int n) {
         
-        int num = n;
-        int c = 0;
+        // int num = n;
+        // int c = 0;
 
-        while(num!=0){
-            num/=10;
-            c++;
-        }
+        // while(num!=0){
+        //     num/=10;
+        //     c++;
+        // }
 
-        int[] arr = new int[c];
-        int ind = 0;
+        // int[] arr = new int[c];
+        // int ind = 0;
+        // while(n!=0){
+        //     arr[ind++] = n%10;
+        //     n/=10;
+        // }
+
+        // if(arr.length == 2) return arr[0]*arr[1];
+        // int maxProduct = Integer.MIN_VALUE;
+        // for(int i=0;i<arr.length;i++){
+        //     for(int j=0;j<arr.length;j++){
+        //         int prod = arr[i];
+        //         if(i!=j) {
+        //             prod*=arr[j];
+        //         System.out.println(arr[i] + " " + arr[j] + " " + prod);
+        //         maxProduct = Math.max(maxProduct,prod);
+        //         }
+        //     }
+        // }
+
+        // return maxProduct;
+
+        int max1 = Integer.MIN_VALUE;
+        int max2 = Integer.MIN_VALUE;
+
         while(n!=0){
-            arr[ind++] = n%10;
+
+            int num = n%10;
+            if(num>max1){
+                max2 = max1;
+                max1 = num;
+            }
+            else if(num > max2){
+                max2 = num;
+            }
             n/=10;
         }
 
-        if(arr.length == 2) return arr[0]*arr[1];
-        int maxProduct = Integer.MIN_VALUE;
-        for(int i=0;i<arr.length;i++){
-            for(int j=0;j<arr.length;j++){
-                int prod = arr[i];
-                if(i!=j) {
-                    prod*=arr[j];
-                System.out.println(arr[i] + " " + arr[j] + " " + prod);
-                maxProduct = Math.max(maxProduct,prod);
-                }
-            }
-        }
-
-        return maxProduct;
+        return max1 * max2;
     }
 }
