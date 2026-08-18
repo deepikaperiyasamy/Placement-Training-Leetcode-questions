a2d9e50c5dd2e750a97f39a3356a62b41d708f11
Deepika Periyasamy <deepikaperiyasamy502@gmail.com>
2026-08-18 10:00:37 +0530

    Time: 10 ms (26.32%) | Memory: 41.9 MB (86.14%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int fib(int n) {
+        
+        return fibo(n);
+    }
+
+    public static int fibo(int n){
+
+        if(n == 0) return 0;
+        if(n == 1) return 1;
+
+        return fibo(n-2) + fibo(n-1);
+    }
+}
